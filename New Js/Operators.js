5a863722dcf858fc5aef43ef274534cb50d15dd3
Abhishekkushwaha2023 <135839243+Abhishekkushwaha2023@users.noreply.github.com>
2023-09-06 12:54:41 +0530

    Create Operators.js

---
+++ New Js/Operators.js
@@ -0,0 +1,215 @@
+//Assignment Operator
+/*
+Operator    Example    Same As
+=           x=y        x=y
++=          x+=y       x=x+y
+-=          x-=y       x=x-y
+*=          x*=y       x=x*y
+/=          x/=y       x=x/y
+%=          x%=y       x=x%y
+^=          x^=y       x=x^y
+|=          x|=y       x=x|y
+**=         x**=y      x=x**y
+*/
+
+/*
+var x = 6;
+var y = 9;
+document.write("Addition\n", x+y);
+document.write("\nSubtrction\n", x-y);
+document.write("\nMultiplication\n", x*y);
+document.write("\nDivision\n", x/y);
+document.write("\nModulus\n", x%y);
+
+
+var z = 5;
+document.write("\nIncrement\n", ++z);
+document.write("\nDecrement\n", --z);
+*/
+
+// var a = 10;
+// var b = a;
+
+// document.write("Value of b", b);
+
+// var a1 = 20;
+// a1+=5;
+// a1 = a1+5;
+// document.write("Value of a1\n", a1);
+
+// a1-=5;
+// document.write("Value of a1\n", a1);
+
+// a3 = 30;
+// a3*=7;
+// document.write("Value of a3\n", a3);
+
+// a4 = 35;
+// a4/=5;
+// document.write("Value of a4\n", a4);
+
+// a5 = 40;
+// a5%=7;
+// document.write("Value of a5\n", a5);
+
+
+
+//String Operator
+
+
+/*
+ Operator       Descript
+ +              Concatenation
+ +=             Same as assignment operator
+*/
+
+
+// var str1 = "Hello";
+// var str2 = "World";
+// document.write(str1+" "+str2);
+
+// var str3 = "Welcome ";
+// str3+="to Simplicode";
+// document.write( str3);
+
+
+// var a = 12;
+// var b = 8;
+// var c = 5;
+// var d = 10;
+// var result = a+b+"hello"+c+d;
+// document.write("<h1>Value of result is \n"+result+"\n Hi there</h1>")
+// document.write("<h1>Value of result is \n"+result+"\n Hi there</h1>")
+// document.write("<h1>Value of result is \n"+result+"\n Hi there</h1>")
+
+
+
+
+
+//Conditional Operator
+
+/*
+Variable name = (condition) ? Value1:value2
+Var voter = (age<18) ? "Too young" : "Old enough";
+*/
+
+// var x = 50;
+// var y = document.write("value is \n", x>=40? "Greater":"Smaller");
+// var y = (x>=40)?"true":"false";
+// document.write(y);
+
+
+
+//Logical Operator
+
+/*
+let x = 7 and y= 4;
+Operator       Description           Example
+&&             and                   (x<10 && y>1)is true
+||             Or                    (x==5 || y==5)is false
+!              not                   !(x==y) is true
+*/
+
+//Comparison Operator
+
+/*
+let x = 5
+Operator  Description                 Comparison    Result 
+==        Equals to                   x = 8         False
+===       Equal value & data type     x===5         True 
+!=        Not equal                   x!=8          True
+!==       Not equal value or data     x!==5         False
+          type                     
+>         Greater than                x>8           False 
+<         Less than                   x<8           True
+>=        Greater than or equals to   x>=8          False
+<=        Less than or equals to      x<=           True
+*/
+/*
+
+var x = 10;
+
+document.write("<br>")
+document.write(x==8);
+
+document.write("<br>")
+document.write(x===8);
+
+document.write("<br>")
+document.write(x!=8);
+
+document.write("<br>")
+document.write(x!==8);
+
+document.write("<br>")
+document.write(x>8);
+
+document.write("<br>")
+document.write(x<8);
+
+document.write("<br>")
+document.write(x>=8);
+
+document.write("<br>")
+document.write(x<=8);
+
+*/
+
+/*
+var x = 17;
+var y = "Hey there!";
+var flag = false;
+var p;
+var abc = null
+
+document.write("<br>")
+document.write(typeof(x));
+
+document.write("<br>")
+document.write(typeof(y));
+
+document.write("<br>")
+document.write(typeof(flag));
+
+document.write("<br>")
+document.write(typeof(p));
+
+document.write("<br>")
+document.write(typeof(abc));
+*/
+
+
+
+//Biwise Operator
+
+
+/*
+let x = 5;
+
+Operator  Description  Example  Same as      Result Decimal
+&         AND          5&1      0101&0001    0001   1
+|         OR           5|1      0101|0001    0101   5
+~         NOT          ~5       ~0101        1010   ~6
+^         XOR          5^1      0101^0001    0100   4
+*/
+
+
+
+
+/*
+var x = 5;
+var y = 1;
+document.write("<br>")
+document.write(x&y);
+
+document.write("<br>")
+document.write(x|y);
+
+document.write("<br>")
+document.write(~y);
+
+document.write("<br>")
+document.write(x^y);
+
+*/
+
