5b07e52080e5b9d5530ceffb826fea63659fbbeb
Abhishekkushwaha2023 <135839243+Abhishekkushwaha2023@users.noreply.github.com>
2023-09-06 11:25:07 +0530

    Create main.js

---
+++ New Js/main.js
@@ -0,0 +1,79 @@
+// let a = 20;
+// let b = 5;
+// let c = 2003;
+// let d = a + b + c;
+// console.log(d);
+// document.writeln("Hello World");
+// document.writeln("Welcome to Simplicode");
+// document.writeln("This is our first program");
+// document.write(45 + 5);
+
+
+// var a = 15;
+// var b = 6;
+// var c = a*b;
+// document.write("the value of c is\n", c);
+// document.write("the value of b is\n", b);
+// document.write("the value of a is\n", a);
+
+
+// let bikeName;
+// bikeName = "Royal Enfield";
+// document.write(bikeName);
+
+// let bikeName = "Ninja";
+// document.write(bikeName);
+
+ // const x = 5, y = 7, z = x + y;
+ // document.write(z);
+
+ 
+// let x = 20, y = 5, z = 2004;
+// let a = x * y * z;
+// document.write(a);
+
+
+var x = 20;
+var y = "hello world";
+var flag = false;
+
+// int x = 5;
+
+var p;
+document.write(typeof(x));
+
+document.write(typeof(y));
+
+document.write(typeof(flag));
+
+document.write(typeof(p));
+
+var num=null;
+document.write(typeof(null));
+
+
+var num = String(34);
+console.log(num , typeof(num));
+
+var str = "Hello";
+console.log(str, typeof(str));
+
+var bool = String(true);
+console.log(bool, typeof(bool));
+
+var str1 = Number("6969");
+str1 = str1+2; 
+console.log(str1, typeof(str1));
+
+var str2 = Number("6969a");
+str2 = str2 +2;
+console.log(str2, typeof(str2));
+
+var bool2 = Number(true);
+console.log(bool2, typeof(bool2));
+
+var number = Number("69.88");
+console.log(number, typeof(number));
+
+var number = parseFloat("72.69");
+console.log(number.toFixed(3), typeof(number));
