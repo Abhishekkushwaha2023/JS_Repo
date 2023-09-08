0d8c5057e77e3ed276aef839f62bbac6788962ab
Abhishekkushwaha2023 <135839243+Abhishekkushwaha2023@users.noreply.github.com>
2023-09-08 11:11:00 +0530

    Create Switch.js

---
+++ New Js/Switch.js
@@ -0,0 +1,93 @@
+// 20 -> seventy-five
+/*
+var month = 7;
+switch(month)
+{
+    case 1:
+        document.write("January");
+        break; n
+        
+
+    case 2:
+        document.write("February");
+        break;
+
+    case 3:
+        document.write("March");
+        break;
+    
+    case 4:
+        document.write("April");
+        break;
+
+    case 5:
+        document.write("May");
+        break;
+
+    case 6:
+        document.write("June");
+        break;
+ 
+
+    case 7:
+        document.write("July");
+        break;    
+        
+    case 8:
+        document.write("August");
+        break;
+
+    case 9:
+        document.write("September");
+        break;
+
+    case 10:
+        document.write("October");
+        break;
+
+    case 11:
+        document.write("November");
+        break;
+
+    case 12:
+        document.write("December");
+        break;
+ 
+    default:
+        document.write("Wrong input");
+
+
+}
+
+*/
+
+/*
+var month = "June";
+switch(month)
+{
+case "June":
+    document.write("Sixth month");
+    break;
+case "July":
+    document.write("Seventh month");
+    break;
+default:
+    document.write("Wrong input");
+}
+
+*/
+/*
+var month = false;
+switch(month)
+{
+case true:
+    document.write(1);
+    break;
+case false:
+    document.write(0);
+    break;
+default:
+    document.write("Wrong input");
+}
+
+*/
