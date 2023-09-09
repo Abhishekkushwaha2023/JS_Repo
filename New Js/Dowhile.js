b30bc251d5cfd9699219320986f33de6b13bdd4b
Abhishekkushwaha2023 <135839243+Abhishekkushwaha2023@users.noreply.github.com>
2023-09-09 08:49:31 +0530

    Create Dowhile.js

---
+++ New Js/Dowhile.js
@@ -0,0 +1,27 @@
+/* 
+var x = 5;
+do{
+document.write("Welcome to AKCode");
+}while(x>5);
+*/
+
+var x = 10;
+do{
+document.write(x+" "+"Welcome to AKCode<br>");
+x--;
+}while(x>5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
