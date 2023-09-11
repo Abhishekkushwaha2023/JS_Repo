8d0f6da05dd7891ad8c161042175b58af6737c2a
Abhishekkushwaha2023 <135839243+Abhishekkushwaha2023@users.noreply.github.com>
2023-09-11 08:40:47 +0530

    Create function.js

---
+++ New Js/function.js
@@ -0,0 +1,10 @@
+function sum(x,y)
+{
+    var total = x+y;
+    return total;
+    //document.write(total);
+
+}
+// var output = sum("Welcome to","AK Code");
+var output = sum(35,15);
+document.write(output);
