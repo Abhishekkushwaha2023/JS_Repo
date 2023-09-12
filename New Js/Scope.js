af40e9af8aea75c941543e77828d9926777a0ed8
Abhishekkushwaha2023 <135839243+Abhishekkushwaha2023@users.noreply.github.com>
2023-09-12 08:25:16 +0530

    Create Scope.js

---
+++ New Js/Scope.js
@@ -0,0 +1,26 @@
+ /*
+ function myfunction()
+{
+    var x = 10;
+   
+}
+
+myfunction();
+document.write("<h1>"+x+"</h1>");
+*/
+
+
+
+
+function myfunction()
+{
+    var1 = 20;
+    var x = 10;
+    document.write("<h1>"+x+"</h1>");    
+ //   document.write("<h1>"+y+"</h1>");    
+
+}
+myfunction();
+document.write(var1);
+
+
