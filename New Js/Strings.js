535aaafbb6110fd499ad4c62d35935ecebeb8081
Abhishekkushwaha2023 <135839243+Abhishekkushwaha2023@users.noreply.github.com>
2023-09-12 08:26:43 +0530

    Create Strings.js

---
+++ New Js/Strings.js
@@ -0,0 +1,9 @@
+/*var x = "hi there!";
+document.write(x);
+document.write(typeof(x));
+*/
+
+// document.write("Hi there \"Welcome\" to AK Code");
+// document.write("It\'s Alright!");
+// document.write("The character \\ is called backslash");
+
