7c295965db4cfe9213f034e58f19e9afe8c90a4e
Abhishekkushwaha2023 <135839243+Abhishekkushwaha2023@users.noreply.github.com>
2023-09-14 09:39:42 +0530

    Create Textbox.js

---
+++ New Js/Textbox.js
@@ -0,0 +1,17 @@
+function Print(){
+    var x = document.getElementById("uname").value;
+    var y = document.getElementById("upass").value;
+    alert("The name is :"+x+"\n"+"The password is :"+y);
+
+    if(y=="Abhi@2433")
+    {
+        alert("Login successful");
+    }
+    else
+    {
+        alert("Wrong Password");
+    }
+}
+
+alert("The name is:"+x+"\n"+"The password is :"+y);
+
