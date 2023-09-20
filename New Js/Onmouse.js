039f95c06779bb849435353efa5ad509d6b65ec3
Abhishekkushwaha2023 <135839243+Abhishekkushwaha2023@users.noreply.github.com>
2023-09-20 09:09:06 +0530

    Create Onmouse.js

---
+++ New Js/Onmouse.js
@@ -0,0 +1,23 @@
+function ChangeSize(){
+    document.getElementById("img1").src = "img.jpg";
+
+}
+
+function ChangeSize2(){
+    document.getElementById("Img1").src = "img.jpg";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
