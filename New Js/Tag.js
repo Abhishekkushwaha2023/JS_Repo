2ced946a360443b6c96a44df41ec5c2c7b52409e
Abhishekkushwaha2023 <135839243+Abhishekkushwaha2023@users.noreply.github.com>
2023-09-20 09:07:21 +0530

    Create Tag.js

---
+++ New Js/Tag.js
@@ -0,0 +1,19 @@
+function Fun2() {
+    var x = document.getElementsByTagName("div");
+    for(var i = 0; i<x.length; i++)
+   {
+        x[i].style.backgroundColor = "Yellow";
+   }
+   
+   
+   /*
+    x[0].style.fontFamily = "Courier New";
+    x[1].style.color = "Blue";
+    x[2].style.fontStyle = "italic";
+    x[3].style.backgroundColor = "Red";
+    */
+}
+
+
+
+
