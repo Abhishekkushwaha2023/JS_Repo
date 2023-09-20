5b5892bff6567fc50f82c0ac0c92bad8c84ba82a
Abhishekkushwaha2023 <135839243+Abhishekkushwaha2023@users.noreply.github.com>
2023-09-20 09:10:51 +0530

    Create Classes.js

---
+++ New Js/Classes.js
@@ -0,0 +1,67 @@
+class Student {
+    constructor(name,age,roll){
+        this.name = name;
+        this.age = age;
+        this.roll = roll;
+    }
+
+    Org(){
+        return ("I am "+ this.name + " from Ak Code");
+    }
+    dob(){
+        return("The date of birth of"+this.name +"is"+(2020-this.age));
+    }
+
+
+static add(a,b){
+    return a+b;
+}
+}
+
+class Student1 extends Student {
+    constructor(name,age,roll,language,section){
+        super(name,age,roll);
+        this.language = language;
+        this.section =section;
+    }
+}
+
+var Abhi = new Student1("Abhi", 35, 2, "Js", "A");
+console.log(Abhi);
+console.log(Student1.add(64,8));
+
+
+
+
+
+
+
+
+/*
+var Abhishek = new Student("Abhishek",21,2);
+console.log(Abhishek);
+console.log(Student.add(10,45)); 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
