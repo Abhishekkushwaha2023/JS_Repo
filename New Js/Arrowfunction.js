4eaed6be790ab8b5c50f33b00d37a31f701daf24
Abhishekkushwaha2023 <135839243+Abhishekkushwaha2023@users.noreply.github.com>
2023-09-21 08:43:18 +0530

    Create Arrowfunction.js

---
+++ New Js/Arrowfunction.js
@@ -0,0 +1,26 @@
+function func1(a,b){
+    return a+b;
+}
+var arrow1 = (a,b) =>a+b;
+
+function func2(){
+    console.log("Welcome to AK Code");
+}
+func2();
+
+var arrow2 = () => {
+    console.log("Welcome to AK Code");
+}
+
+var arrow3 = newname => {
+    console.log("hey there ! "+newname);
+}
+
+
+arrow3("Abhi");
+
+
+
+
+
+
