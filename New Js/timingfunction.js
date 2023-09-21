41a657ff267c88289938eb96b58a7b822c1f16b1
Abhishekkushwaha2023 <135839243+Abhishekkushwaha2023@users.noreply.github.com>
2023-09-21 08:44:27 +0530

    Create timingfunction.js

---
+++ New Js/timingfunction.js
@@ -0,0 +1,21 @@
+/*
+function Start(){
+    setTimeout(function(){
+        alert("Welcome Back!");
+    },2000)
+}
+*/
+
+
+var id = 0;
+var message = () => alert("Welcome to Ak Code");
+
+function Start(){
+    id = setInterval(message,5000);
+}
+
+function Stop(){
+    clearTimeout(id);
+}
+
+
