438591d89f7239a64e1509957e24e77b89dcc289
Abhishekkushwaha2023 <135839243+Abhishekkushwaha2023@users.noreply.github.com>
2023-09-24 08:46:48 +0530

    Create character.js

---
+++ New Js/character.js
@@ -0,0 +1,35 @@
+/*
+var regex = /[^def]def/
+var str1 = "This string contains def";
+var output = regex.test(str1);
+console.log(output);
+*/
+
+
+function employee()
+{
+    var empid = document.getElementById("eid").value;
+    var check = /[DE]253[10-99]/
+    if((check.test(empid)))
+    {
+        alert("Valid employee ID")
+    }
+    else
+    {
+        alert("Invalid employee ID");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
